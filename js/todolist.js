765b9b28e40879ab7b5f3e19638bbfd1ba92d261
devmkcode <dev.mkcode@gmail.com>
2021-05-27 18:57:50 +0200

    Add complete to localstorage

--- js/todolist.js
+++ js/todolist.js
@@ -8,31 +8,37 @@ if(window.localStorage.getItem("todos") == undefined){
      window.localStorage.setItem("todos", JSON.stringify(todos));
 }
 
+if(window.localStorage.getItem("statusLocal") == undefined){
+    const statusLocal = [];
+    window.localStorage.setItem("statusLocal", JSON.stringify(statusLocal));
+}
+
 const todosEX = window.localStorage.getItem("todos");
 const todos = JSON.parse(todosEX);
-
+const statusLocal = JSON.parse(window.localStorage.getItem("statusLocal"));
 class item{
 	constructor(name){
 		this.createItem(name);
 	}
     createItem(name){
-    	var itemBox = document.createElement('div');
+
+    	const itemBox = document.createElement('div');
         itemBox.classList.add('item');
 
-    	var input = document.createElement('input');
+    	const input = document.createElement('input');
     	input.type = "text";
     	input.disabled = true;
     	input.value = name;
     	input.classList.add('item_input');
 
-     const complete = document.createElement('button');
-    	complete.classList.add('complete');
-    	complete.innerHTML = "Done";
-    	complete.addEventListener('click', () => {
-      input.classList.toggle('completed');  
-      });
+        const complete = document.createElement('button');
+            complete.classList.add('complete');
+            complete.innerHTML = "Done";
+            complete.addEventListener('click', () => {
+            this.complete(name, input);
+        });
 
-    	var edit = document.createElement('button');
+    	const edit = document.createElement('button');
     	edit.classList.add('edit');
     	edit.innerHTML = "EDIT";
     	edit.addEventListener('click', () => {
@@ -42,13 +48,20 @@ class item{
           if(e.keyCode == 13){
           edit.click();
           }
-          })
-     });
+          });
+        });
 
-    	var remove = document.createElement('button');
+    	const remove = document.createElement('button');
     	remove.classList.add('remove');
     	remove.innerHTML = "REMOVE";
     	remove.addEventListener('click', () => this.remove(itemBox, name));
+
+        let index = todos.indexOf(name);
+        if(statusLocal[index] === "completed"){
+            input.classList.add("completed");
+        }else{
+            input.classList.remove("completed");
+        }
 
     	container.appendChild(itemBox);
 
@@ -71,11 +84,26 @@ class item{
         }
     }
 
+    complete(name, input){
+        let index = todos.indexOf(name);
+        if(statusLocal[index] === "uncompleted"){
+            statusLocal[index] = 'completed';
+            input.classList.add('completed');  
+            window.localStorage.setItem("statusLocal", JSON.stringify(statusLocal));
+        }else{
+            statusLocal[index] = 'uncompleted';
+            input.classList.remove('completed');  
+            window.localStorage.setItem("statusLocal", JSON.stringify(statusLocal));
+        }
+    }
+
     remove(itemBox, name){
         itemBox.parentNode.removeChild(itemBox);
         let index = todos.indexOf(name);
         todos.splice(index, 1);
+        statusLocal.splice(index, 1);
         window.localStorage.setItem("todos", JSON.stringify(todos));
+        window.localStorage.setItem("statusLocal", JSON.stringify(statusLocal));
     }
 }
 
@@ -84,14 +112,17 @@ add.addEventListener('click', check);
 function check(e){
      e.preventDefault();
 	if(inputValue.value != ""){
-	new item(inputValue.value);
+       
+	    new item(inputValue.value);
         todos.push(inputValue.value);
         window.localStorage.setItem("todos", JSON.stringify(todos));
+        statusLocal.push('uncompleted');
+        window.localStorage.setItem("statusLocal", JSON.stringify(statusLocal));
 		inputValue.value = "";
 	}
 }
 
 
-for (var v = 0 ; v < todos.length ; v++){
+for (let v = 0 ; v < todos.length ; v++){
     new item(todos[v]);
 }
